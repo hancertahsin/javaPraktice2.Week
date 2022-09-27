cb0a66aec5cd1852bb595b8ff3c456fd124f5916
Tahsin <105295833+hancertahsin@users.noreply.github.com>
2022-09-27 22:59:55 +0300

    Add files via upload

---
+++ class/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //referans type
+        CustomerManager customerManager;
+        CustomerManager customerManager2 = new CustomerManager();
+        customerManager = customerManager2;
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
+    }
+}
