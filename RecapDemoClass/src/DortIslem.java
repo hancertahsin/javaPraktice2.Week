ec43f25dc34f24ca6bfd12e8a90766c1b925fa65
Tahsin <105295833+hancertahsin@users.noreply.github.com>
2022-09-28 13:23:10 +0300

    Add files via upload

---
+++ RecapDemoClass/src/DortIslem.java
@@ -0,0 +1,18 @@
+public class DortIslem {
+    public int topla(int sayı1, int sayı2) {
+        return sayı1 + sayı2;
+    }
+
+    public int Cikar(int sayı1, int sayı2) {
+        return sayı1 - sayı2;
+    }
+
+    public int Carp(int sayı1, int sayı2) {
+        return sayı1 * sayı2;
+    }
+
+    public int Bol(int sayı1, int sayı2) {
+        return sayı1 / sayı2;
+    }
+
+}
