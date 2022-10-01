a65c40a184449122b8ea55fcbc0d2625c9c3df20
Tahsin <105295833+hancertahsin@users.noreply.github.com>
2022-10-01 15:20:31 +0300

    Add files via upload

---
+++ methodOverloading/src/Main.java
@@ -0,0 +1,10 @@
+    public class Main {
+
+        public static void main(String[] args){
+
+            Dortislem dortislem=new Dortislem();
+            dortislem.topla(2,3);
+            System.out.println(dortislem.topla(2,3));
+            dortislem.topla(2,3,5);
+        }
+}
